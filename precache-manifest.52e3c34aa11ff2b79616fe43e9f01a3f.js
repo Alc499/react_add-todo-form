2bdfc43ede515f97b60f7f6d519dcbac9fb07f12
Alc499 <Ali.vitchenko@gmail.com>
2019-07-22 19:30:32 +0300

    Updates

---
+++ precache-manifest.52e3c34aa11ff2b79616fe43e9f01a3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abed3d07e69f2b69498e92bc5a5082e",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "268bb4e55a42ca79f984",
+    "url": "/react_add-todo-form/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "9d1094e3d6f8817a0fe4",
+    "url": "/react_add-todo-form/static/js/2.d9d1bc8f.chunk.js"
+  },
+  {
+    "revision": "268bb4e55a42ca79f984",
+    "url": "/react_add-todo-form/static/js/main.91998361.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
